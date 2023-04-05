b239fa0c7f1e681a8b11e4ca9a2db611d5a37f6e
Petrut <100096585+PepeTheBuilder@users.noreply.github.com>
2023-04-06 00:59:38 +0300

    Update a1.c

--- a1/a1.c
+++ a1/a1.c
@@ -8,42 +8,74 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
 #define MAX_PATH_LEN 4096
-#define search_ECHO 1
+#define search_ECHO 0
+
+int recursive = 0;
+char *filtering_options = "";
+int filtering = 0;
+int perm_exe = 0;
+char *dir_path="";
 
 
 void listDir(char *dirName){
 
-	DIR* dir;
+	DIR *dir;
 	struct dirent *dirEntry;
 	struct stat inode;
 	char name[MAX_PATH_LEN];
 
 	dir = opendir(dirName);
-	if (dir == 0) {
-		printf("Error opening directory");
+	if (dir == 0)
+	{
+		//printf("Error opening directory");
 		exit(4);
 	}
 
-	// iterate the directory contents
-	while ((dirEntry=readdir(dir)) != 0) {
-		// build the complete path to the element in the directory
-		snprintf(name, MAX_PATH_LEN, "%s/%s",dirName,dirEntry->d_name);
-		
-		lstat (name, &inode);
+	while ((dirEntry = readdir(dir)) != 0)
+	{
+		snprintf(name, MAX_PATH_LEN, "%s/%s", dirName, dirEntry->d_name);
+
+		lstat(name, &inode);
+		if (strcmp(dirEntry->d_name, ".") == 0 || strcmp(dirEntry->d_name, "..") == 0){
+			continue;
+		}
+
+
+		printf("%s/%s\n", dirName,dirEntry->d_name);
+	}
+
+	closedir(dir);
+}
+void listDirRecusive(char *dirName)
+{
+	DIR *dir;
+	struct dirent *dirEntry;
+	struct stat inode;
+	char name[MAX_PATH_LEN];
+
+	dir = opendir(dirName);
+	if (dir == 0)
+	{
+		//perror("Error opening directory");
+		exit(4);
+	}
+	listDir(dirName);
+
+	while ((dirEntry = readdir(dir)) != 0)
+	{
+
+		snprintf(name, MAX_PATH_LEN, "%s/%s", dirName, dirEntry->d_name);
+
+		lstat(name, &inode);
 
 		if (S_ISDIR(inode.st_mode))
-			printf("dir ");
-		else 
-			if (S_ISREG(inode.st_mode))
-				printf ("file ");
-			else
-				if (S_ISLNK(inode.st_mode))
-					printf ("lnk ");
-				else;
-
-		printf(" %s\n", dirEntry->d_name);
+		{
+			if (strcmp(dirEntry->d_name, ".") != 0 && strcmp(dirEntry->d_name, "..") != 0)
+			{
+				listDirRecusive(name);
+			}
+		}
 	}
 
 	closedir(dir);
@@ -51,217 +83,185 @@ void listDir(char *dirName){
 
 int search_dir(char *dir_name, char *searched_name){
 
-	int gasit=0;
-	DIR* dir;
+
+	int gasit = 0;
+	DIR *dir;
 	struct dirent *dirEntry;
 	struct stat inode;
 	char name[MAX_PATH_LEN];
 	dir = opendir(dir_name);
-	if (dir == 0) {
-
-		printf("Error opening directory %s\n", dir_name);
-		//exit(4);
+	if (dir == 0)
+	{
+		if (search_ECHO)printf("Error opening directory %s\n", dir_name);
+		// exit(4);
 		return 0;
 	}
 
-	// iterate the directory contents
-	while ((dirEntry=readdir(dir)) != 0) {
-		// build the complete path to the element in the directory
-		snprintf(name, MAX_PATH_LEN, "%s/%s",dir_name,dirEntry->d_name);
-		
-		// get info about the directory's element
-		lstat (name, &inode);
+	while ((dirEntry = readdir(dir)) != 0)
+	{
+		snprintf(name, MAX_PATH_LEN, "%s/%s", dir_name, dirEntry->d_name);
+		lstat(name, &inode);
 
-		// test the type of the directory's element
+		if (strstr(dirEntry->d_name, searched_name))
+		{
 
-		if (strstr(dirEntry->d_name,searched_name)){
-		
-		if (S_ISDIR(inode.st_mode)){
-			if(search_ECHO)printf("dir ");
-			gasit=1;
-		}
-		else 
-			if (S_ISREG(inode.st_mode)){
-				if(search_ECHO)printf ("file ");
-				gasit=2;
+			if (S_ISDIR(inode.st_mode))
+			{
+				if (search_ECHO)
+					printf("dir ");
+				gasit = 1;
 			}
-			else
-				if (S_ISLNK(inode.st_mode)){
-					if(search_ECHO)printf ("lnk ");
-					gasit=3;
-				}
-		lstat (name, &inode);
-		if(search_ECHO)printf("%s\n", dirEntry->d_name);
-		if(search_ECHO)printf("%s\n", name);
+			else if (S_ISREG(inode.st_mode))
+			{
+				if (search_ECHO)
+					printf("file ");
+				gasit = 2;
+			}
+			else if (S_ISLNK(inode.st_mode))
+			{
+				if (search_ECHO)
+					printf("lnk ");
+				gasit = 3;
+			}
+			lstat(name, &inode);
+			//if (search_ECHO)
+				//printf("%s\n", dirEntry->d_name);
+			//if (search_ECHO)
+				printf("%s\n", name);
 			return gasit;
 		}
 	}
 
 	closedir(dir);
-	if(search_ECHO)printf("nope \n");
+	if (search_ECHO)
+		printf("nope \n");
 	return 0;
 }
 
 int search_tree(char *dir_name, char *searched_name){
-	int search=search_dir(dir_name,searched_name);
-	if(search!=0){
+	int search=0;
+	if(filtering==0)
+	listDirRecusive(dir_path);
+	else
+	search=search_dir(dir_path,filtering_options);
+	if (search != 0)
+	{
 		return 1;
 	}
-	
-	DIR* dir;
+
+	DIR *dir;
 	struct dirent *dirEntry;
 	struct stat inode;
 	char name[MAX_PATH_LEN];
 	dir = opendir(dir_name);
-	if(dir!=0)
-	while ((dirEntry=readdir(dir)) != 0) {
-		// build the complete path to the element in the directory
-		snprintf(name, MAX_PATH_LEN, "%s/%s",dir_name,dirEntry->d_name);
-		
-		// get info about the directory's element
-		lstat (name, &inode);
+	if (dir != 0)
+		while ((dirEntry = readdir(dir)) != 0)
+		{
+			// build the complete path to the element in the directory
+			snprintf(name, MAX_PATH_LEN, "%s/%s", dir_name, dirEntry->d_name);
 
-		if (S_ISDIR(inode.st_mode)){
-			if(strcmp(dirEntry->d_name,".")!=0 && strcmp(dirEntry->d_name,"..")!=0){
-				int aux =search_tree(name,searched_name);
-				if(aux==1){
-					printf("Directory founded: %s\n", name);
-					return 10;
-				}
-					
-				if(aux==2){
-					printf("File found");
-					return 10;
-                }
-				if(aux==10){
-					return 10;
+			// get info about the directory's element
+			lstat(name, &inode);
+
+			if (S_ISDIR(inode.st_mode))
+			{
+				if (strcmp(dirEntry->d_name, ".") != 0 && strcmp(dirEntry->d_name, "..") != 0)
+				{
+					int aux = 0;
+						if(filtering==0){
+						listDirRecusive(name);
+						//search_tree(name,filtering_options);
+						//aux=1;
+						}
+						else
+						search=search_dir(name,filtering_options);
+					if (aux == 1)
+					{
+						//printf("Directory founded: %s\n", name);
+						return 10;
+					}
+
+					if (aux == 2)
+					{
+						//printf("File found");
+						return 10;
+					}
+					if (aux == 10)
+					{
+						return 10;
+					}
 				}
 			}
-			continue;
 		}
-	}
 
 	closedir(dir);
 	return 0;
 }
-/*
-int readDirections(int argc, char **argv){
-   
 
-   if(strcmp(argv[1],"variant")==0){
-    printf("22794");
-   }
-       
-    if(strcmp(argv[1],"list")==0){
+int readDirections(int argc, char **argv)
+{
+	if (strcmp(argv[1], "variant") == 0)
+	{
+		printf("22794");
+	}
+	else if (strcmp(argv[1], "list") == 0)
+	{
+		if (argc < 2)
+		{
+			printf("Usage: %s list path=<dir_path> [recursive] [<filtering_options>]\n", argv[0]);
+			return 0;
+		}
 
+		for(int i = 2; i < argc; i++)
+		{
+			if (strncmp(argv[i], "path=", 5) == 0)
+			{
+				dir_path = &argv[i][5];
+			}
+			else if (strcmp(argv[i], "recursive") == 0)
+			{
+				recursive = 1;
+			}
+			else if (strncmp(argv[i], "name_ends_with=", 15) == 0)
+			{
+				filtering_options = &argv[i][15];
+				filtering=1;
+			}
+			if (strcmp(argv[i], "has_perm_execute") == 0)
+			{
+				perm_exe = 1;
+			}
+		}
+		printf("SUCCESS\n");
+		//printf("recusrive:%d, perm_exe:%d, filtering:%s, path=%s \n",recursive,perm_exe,filtering_options, dir_path);
+		
+		if(recursive==1)
+		{
+			listDirRecusive(dir_path);
+		}
+		else {
+			//search_dir(dir_path,filtering_options);
+			if(filtering==0)
+			listDir(dir_path);
+			else
+			search_dir(dir_path,filtering_options);
+		}
+	}
 
-        if(argc<=4 && argc>2){
-        printf("%d\n",argc);
-        int recusive=0; 
-        int  permision=0; 
-        int end=0; 
-        char end_with[100];
-        char path[4096];
-         
-        for(int i=2; i<=argc;i++){
-            
-        if(strcmp(argv[i],"recusive")==0){
-            recusive=1;
-            break;
-        }
-        }
-           printf("aici3\n");
-
-        for(int i=2; i<=argc;i++){
-            printf("aici i=%d\n",i);
-        if(strcmp(argv[i],"has_perm_execute")==0){
-            permision=1;
-            break;
-        }
-        }
-         printf("aici4\n");
-        for(int i=2; i<=argc;i++){
-            if(strcmp(argv[i],"name_ends_with=")==0){
-                end=1;
-                strcpy(end_with,argv[i]+15);
-                printf("%s, ",end_with);
-                break;
-            }
-        }
-        for(int i=2; i<=argc;i++){
-            if(strstr(argv[i],"path=")==0){
-                strcpy(path,argv[i]+5);
-                printf("%s, ",path);
-                break;
-            }
-        }
-        printf("recusive %d, permision =%d, end=%d",recusive,permision,end);
-        }
-
-    }
-    return 0;
-
-}
-*/
-
-int readDirections(int argc, char **argv) {
-    if(strcmp(argv[1], "variant") == 0) {
-        printf("22794");
-    } else if(strcmp(argv[1], "list") == 0) {
-        if(argc <= 4 && argc > 2) {
-            int recursive = 0;
-            int permission = 0;
-            int end = 0;
-            char end_with[100];
-            char path[4096];
-
-            for(int i = 2; i < argc; i++) {
-                if(strcmp(argv[i], "recursive") == 0) {
-                    printf("aici");
-                    recursive = 1;
-                    break;
-                }
-            }
-
-            for(int i = 2; i < argc; i++) {
-                if(strcmp(argv[i], "has_perm_execute") == 0) {
-                    permission = 1;
-                    break;
-                }
-            }
-
-
-            for(int i = 2; i < argc; i++) {
-                if(strncmp(argv[i], "name_ends_with=", 15) == 0) {
-                    end = 1;
-                    strcpy(end_with, argv[i] + 15);
-                    printf("%s, ", end_with);
-                    break;
-                }
-            }
-
-            for(int i = 2; i < argc; i++) {
-                if(strstr(argv[i], "path=") != NULL) {
-                    strcpy(path, argv[i] + 5);
-                    printf("%s, ", path);
-                    break;
-                }
-            }
-
-            printf("recursive=%d, permission=%d, end=%d\n", recursive, permission, end);
-        }
-    }
-    return 0;
+	return 0;
 }
 
-int main(int argc, char **argv){
-    if(argc < 1 || argv == NULL) {
-        printf("Invalid arguments\n");
-        return -1;
-    }
-    else {
-         readDirections(argc, argv);
-    }
+int main(int argc, char **argv)
+{
+	if (argc < 1 || argv == NULL)
+	{
+		printf("Invalid arguments\n");
+		return -1;
+	}
+	else
+	{
+		readDirections(argc, argv);
+	}
 
-    return 0;
+	return 0;
 }
