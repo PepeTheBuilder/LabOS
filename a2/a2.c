358225e2ff35914f0abf7cce8a9384f56b1fb44d
Petrut <100096585+PepeTheBuilder@users.noreply.github.com>
2023-05-03 13:21:22 +0300

    Update a2.c
    42p ex1 correct ex2 partial 3p out of 5
            Checking process hierarchy...
                    CORRECT         [5 point(s)]
            Checking threads from the same process...
                    thread T8.1 starts after T8.2
                    PARTIAL CORRECT [3 point(s)]
            Checking threads barrier...
                    missing thread(s) 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 in process 2
                    FAIL            [0 point(s)]
            Checking threads from different processes...
                    missing threads 1 2 3 4 5 in process 6
                    FAIL            [0 point(s)]

--- a2/a2.c
+++ a2/a2.c
@@ -6,6 +6,43 @@
 #include <stdlib.h>
 #include "a2_helper.h"
 
+void *t8(void *arg){
+    int i = *(int*) arg;
+    info(BEGIN,8,i);
+    info(END,8,i); 
+    return NULL;
+}
+
+void *t2(void *arg){
+    int i = *(int*) arg;
+    info(BEGIN,2,i);
+    info(END,2,i); 
+    return NULL;
+}
+
+void *p8(void *arg){
+    info(BEGIN,8,0);
+
+    pthread_t thread[5];
+    for(int i=1; i<=4; i++){
+        int* arg = malloc(sizeof(*arg));
+        *arg = i;
+        pthread_create(&thread[i], NULL, t8, arg);
+        if(i!=1){ 
+            pthread_join(thread[i], NULL);
+        }
+        if(i==2) {
+            pthread_join(thread[2], NULL);
+            pthread_join(thread[1], NULL);
+        }
+       
+    }
+
+    info(END,8,0);    
+
+    return NULL;
+}
+
 void *p7(void *arg){
     info(BEGIN,7,0);
 
@@ -29,15 +66,14 @@ void *p6(void *arg){
     return NULL;
 }
 
-void *p8(void *arg){
-    info(BEGIN,8,0);
+void *p5(void *arg){
+    info(BEGIN,5,0);
 
-    info(END,8,0);    
-
+    info(END,5,0);
     return NULL;
 }
 
-void *p4 (void *arg) {
+void *p4(void *arg){
     info(BEGIN, 4, 0);
 
     pid_t pid6, pid8;
@@ -59,7 +95,7 @@ void *p4 (void *arg) {
     return NULL;
 }
 
-void *p3 (void *arg){
+void *p3(void *arg){
     info(BEGIN,3,0);
     info(END,3,0);    
     return NULL;
@@ -87,14 +123,20 @@ void *p2(void *arg){
     }
     waitpid(pid4, NULL, 0);
 
+    // pthread_t thread[40];
+    // for(int i=1; i<=40; i++){
+    //     int* arg = malloc(sizeof(*arg));
+    //     *arg = i;
+    //     pthread_create(&thread[i], NULL, t2, arg);
+
+    //     pthread_join(thread[i], NULL);
+    //     if(i == 11){
+    //         pthread_join(thread[11],NULL);
+    //     }
+    
+    // }
+
     info(END,2,0);
-    return NULL;
-}
-
-void *p5(void *arg){
-    info(BEGIN,5,0);
-
-    info(END,5,0);
     return NULL;
 }
 
@@ -126,9 +168,13 @@ int main(){
     init();
 
     info(BEGIN, 1, 0);
+
     pthread_t root_thread;
     pthread_create(&root_thread, NULL, p1, NULL);
     pthread_join(root_thread, NULL);
+
+
     info(END, 1, 0);
+
     return 0;
 }
