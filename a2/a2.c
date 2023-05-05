55772c93779fdb496c6932b5283d995a350d474a
Petrut <100096585+PepeTheBuilder@users.noreply.github.com>
2023-05-06 00:18:41 +0300

    Update a2.c
    se fac din de toate dar ultimele doua nu ii sigur ca merg
    intre 70p si 90p

--- a2/a2.c
+++ a2/a2.c
@@ -5,10 +5,13 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include "a2_helper.h"
 
-sem_t sem,thread11;
-int ok=0;
+sem_t sem,thread11,*t6_complete,*t8_end;
+pthread_t tredu;
+int ok=0,end8=0,end6=0;
+
 void P(sem_t *sem)
 {
     sem_wait(sem);
@@ -19,34 +22,88 @@ void V(sem_t *sem)
     sem_post(sem);
 }
 
-void *t8(void *arg){
+void *t2(void *arg){
     int i = *(int*) arg;
-    info(BEGIN,8,i);
-    if(i==1){
-        pthread_t thread2 ;
+    P(&sem);
+    info(BEGIN,2,i);
+    if(i==1||i==2||i==3||i==11){
+        V(&thread11);
+        while(ok==0&&i!=11){
+            
+        }
+        P(&thread11);
+    }
+    info(END,2,i);
+    V(&sem);
+    pthread_exit(NULL);
+}
+
+void *t6(void *arg) {
+    int i = *(int*) arg;
+    if (i == 2) {
+        printf("astept pt end8\n");
+        P(t8_end);
+        printf("GATA end8 \n");
+    }
+    info(BEGIN, 6, i);
+
+    info(END, 6, i);
+    if (i == 5) {
+        end6 = 1;
+        printf("end6 = 1\n");
+        V(t6_complete); // signal completion of p6 thread[6]
+    }
+    pthread_exit(NULL);
+}
+
+void *t8(void *arg) {
+    int i = *(int*) arg;
+    if (i == 3) {
+        printf("astept pt end6\n");
+        P(t6_complete); // wait for completion of p6 thread[6]
+        printf("GATA end6 \n");
+    }
+    if(i==2){
+        
+    }
+    info(BEGIN, 8, i);
+    if (i == 1) {
+        pthread_t thread2;
         int* aux = malloc(sizeof(*arg));
-        *aux=2;
+        *aux = 2;
         pthread_create(&thread2, NULL, t8, aux);
         pthread_join(thread2, NULL);
+        info(END, 8, 1);
+        pthread_exit(NULL);
+    }else
+    info(END, 8, i);
+    if (i == 3) {
+        V(t8_end);
+        printf("end8=1\n");
     }
-    info(END,8,i); 
-    return NULL;
+    pthread_exit(NULL);
 }
+
 
 void *p8(void *arg){
     info(BEGIN,8,0);
 
-    pthread_t thread[3];
+    pthread_t thread[4];
     int* aux = malloc(sizeof(*arg));
     *aux=1;
+    printf("Incep tredu 1\n\n");
     pthread_create(&thread[0], NULL, t8, aux);
-    pthread_join(thread[0], NULL);
     for(int i=3; i<5; i++){
         int* arg = malloc(sizeof(*arg));
         *arg = i;
         pthread_create(&thread[i-2], NULL, t8, arg);
-        pthread_join(thread[i-2], NULL);
     }
+
+    // wait for all threads in p8 to complete
+    pthread_join(thread[1], NULL);
+    pthread_join(thread[2], NULL);
+    pthread_join(thread[0], NULL);
+    
 
     info(END,8,0);    
 
@@ -70,8 +127,25 @@ void *p6(void *arg){
         p7(NULL);
         exit(0);
     }
-    waitpid(pid7, NULL, 0);
+    pthread_t thread[6];
+    for(int i=1; i<6; i++){
 
+        int* aux = malloc(sizeof(*arg));
+        *aux = i;
+        pthread_create(&thread[i], NULL, t6, aux);
+    }
+    for(int i=1; i<6; i+=4){ 
+        if(i==3)continue; 
+        pthread_join(thread[i],NULL);
+        
+    }
+    tredu=thread[3];
+
+    waitpid(pid7, NULL, 0);
+    
+    // wait for threads in p8 to complete
+    pthread_join(tredu, NULL);
+    
     info(END,6,0);    
     return NULL;
 }
@@ -87,20 +161,20 @@ void *p4(void *arg){
     info(BEGIN, 4, 0);
 
     pid_t pid6, pid8;
-
     pid6 = fork();
     if (pid6 == 0) {
         p6(NULL);
-        exit(0);
+        exit(0);        
     }
-    waitpid(pid6, NULL, 0);
-
     pid8 = fork();
     if (pid8 == 0) {
         p8(NULL);
         exit(0);
+
     }
-    waitpid(pid8, NULL, 0);
+    
+    waitpid(pid8, NULL, 0);printf("Ba sa facut procesul 8\n\n");
+    waitpid(pid6, NULL, 0);printf("Ba sa facut procesul 6\n\n");
 
     info(END, 4, 0);
     return NULL;
@@ -109,22 +183,6 @@ void *p4(void *arg){
 void *p3(void *arg){
     info(BEGIN,3,0);
     info(END,3,0);    
-    return NULL;
-}
-
-void *t2(void *arg){
-    int i = *(int*) arg;
-    P(&sem);
-    info(BEGIN,2,i);
-    if(i==1||i==2||i==3||i==11){
-        V(&thread11);
-        while(ok==0&&i!=11){
-            
-        }
-        P(&thread11);
-    }
-    info(END,2,i);
-    V(&sem);
     return NULL;
 }
 
@@ -223,13 +281,19 @@ int main(){
     init();
 
     info(BEGIN, 1, 0);
+    t6_complete=sem_open("t6.3_done",O_CREAT,0644, 0);
+    t8_end=sem_open("p8.3_done",O_CREAT,0644, 0);
+    
 
     pthread_t root_thread;
     pthread_create(&root_thread, NULL, p1, NULL);
     pthread_join(root_thread, NULL);
 
-
     info(END, 1, 0);
 
+    sem_close(t6_complete);
+    sem_close(t8_end);
+    unlink("/dev/shm/t6.3_done");
+    unlink("/dev/shm/p8.3_done");
     return 0;
 }
