cf7ffcd96f6d825a449f7db5ba81a7760d7c7e3e
Petrut <100096585+PepeTheBuilder@users.noreply.github.com>
2023-05-23 21:10:01 +0300

    habar n-am ce fac aici

--- a3/a3.c
+++ a3/a3.c
@@ -9,10 +9,10 @@
 
 char FILE_WRITE[] = "RESP_PIPE_22794";
 char FILE_READ[] =  "REQ_PIPE_22794";
-char con[]="HELLO\0";
+char con[]="HELLO$";
 int sizecon = 7;
-int nr_meu = 22794;
-char mesaj[200]="";
+int varianta = 22794;
+char buff[200]="";
 int dim_cuv = 0;
 int shared_mem;
 char *sharedMem = NULL;
@@ -25,17 +25,17 @@ int main(int argc, char **argv){
 
     int fdr, fdw; 
     if(mkfifo(FILE_WRITE, 0777) != 0) {
-        perror("ERROR\ncannot create/open pipe mkfifo\n");
+        perror("ERROR\ncannot create the response pipe | cannot open the request pipe mkfifo\n");
     } 
     
     fdr = open(FILE_READ, O_RDONLY);
     if(fdr == -1) {
-        perror("ERROR\ncannot create/open pipe fdr\n");
+        perror("ERROR\ncannot create the response pipe | cannot open the request pipe fdr\n");
     }
     
     fdw = open(FILE_WRITE, O_WRONLY);   
     if(fdr == -1) {
-        perror("ERROR\ncannot create/open pipe fdw\n");
+        perror("ERROR\ncannot create the response pipe | cannot open the request pipe fdw\n");
     }
 
 
@@ -43,19 +43,37 @@ int main(int argc, char **argv){
         if(write(fdw, &con, sizecon)==sizecon){
             printf("SUCCESS\n");
         }
+        char aux=' ';
+        int i =0;
+        while(aux!='$'){
+            read(fdr, &aux,1);
+            buff[i]=aux;
+            i++;
+        }
+        //if(strncmp(buff,"VARIANT$",9)==0){
+        char text[]= "VARIANT$";
+        char text2[]="VALUE$";
+        char five='5';
+        write(fdw, &text, 9);
+        write(fdw, &five,1);
+        write(fdw, &varianta,1);
+        write(fdw, &text2, 7);
+            //printf("%s\n",text);
+        //}
+        
     }
     
     
    
-    if(strstr(mesaj, "EXIT")) {		
-            munmap(&shared_mem, 2878785);
-    	    munmap(data, dim_fisier);
-            shm_unlink("/dURY42tR");
-            close(fd1);		           
-            close(fdr);
-            close(fdw);
-            unlink(FILE_WRITE);
-        }
+    // if(strstr(mesaj, "EXIT")) {		
+    //         // munmap(&shared_mem, 2878785);
+    // 	    // munmap(data, dim_fisier);
+    //         // shm_unlink("/nJx8bhQ3");
+    //         close(fd1);		           
+    //         close(fdr);
+    //         close(fdw);
+    //         unlink(FILE_WRITE);
+    //     }
     
     return 0;
 }
