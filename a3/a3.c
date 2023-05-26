071af0d83d83adc54e618fc00c9ab285d2bc34da
Petrut <100096585+PepeTheBuilder@users.noreply.github.com>
2023-05-26 21:02:06 +0300

    merge cititul

--- a3/a3.c
+++ a3/a3.c
@@ -7,73 +7,92 @@
 #include <unistd.h>
 #include <string.h>
 
-char FILE_WRITE[] = "RESP_PIPE_22794";
-char FILE_READ[] =  "REQ_PIPE_22794";
-char con[]="HELLO$";
-int sizecon = 7;
+#define FILE_WRITE "RESP_PIPE_22794"
+#define FILE_READ "REQ_PIPE_22794"
+#define VARIANT "VARIANT$"
+#define VALUE "VALUE$"
+#define HELLO "HELLO$"
+
 int varianta = 22794;
-char buff[200]="";
-int dim_cuv = 0;
-int shared_mem;
-char *sharedMem = NULL;
-//2.6
-int fd1=-1;
-char *data = NULL;
-off_t dim_fisier;
+char aux;
+char msg[200] = "";
+int dimensionMsg = 0;
+int i = 0;
 
-int main(int argc, char **argv){ 
+int main(int argc, char **argv)
+{
 
-    int fdr, fdw; 
-    if(mkfifo(FILE_WRITE, 0777) != 0) {
+    int fdr, fdw;
+    if (mkfifo(FILE_WRITE, 0777) != 0)
+    {
         perror("ERROR\ncannot create the response pipe | cannot open the request pipe mkfifo\n");
-    } 
-    
+        return 1;
+    }
+
     fdr = open(FILE_READ, O_RDONLY);
-    if(fdr == -1) {
+    if (fdr == -1)
+    {
         perror("ERROR\ncannot create the response pipe | cannot open the request pipe fdr\n");
+        return 1;
     }
-    
-    fdw = open(FILE_WRITE, O_WRONLY);   
-    if(fdr == -1) {
+    else
+    {
+        // printf("Am deschis fdr");
+    }
+
+    fdw = open(FILE_WRITE, O_WRONLY);
+    if (fdw == -1)
+    {
         perror("ERROR\ncannot create the response pipe | cannot open the request pipe fdw\n");
+        return 1;
+    }
+
+    if (write(fdw, HELLO, 7) == 7)
+    {
+        printf("SUCCESS\n");
+    }
+    else
+    {
+        printf("NUUUUUUUUU\n");
     }
 
 
-    if(write(fdw, &sizecon, 1) == 1) {
-        if(write(fdw, &con, sizecon)==sizecon){
-            printf("SUCCESS\n");
+    while (1)
+    {
+        char buffer[250];
+        int bytesRead;
+        int m=0;
+        while ((bytesRead = read(fdr, &buffer[m], 1)) > 0)
+        {
+            printf("%c",buffer[m]);
+            if (buffer[m] == '$')
+            {
+                break;
+            }
+            m++;
         }
-        char aux=' ';
-        int i =0;
-        while(aux!='$'){
-            read(fdr, &aux,1);
-            buff[i]=aux;
-            i++;
+        buffer[m]='\0';
+        // printf("Am citit:%s",buffer);
+
+        if (strstr(msg, "VARIANT$"))
+        {
+            write(fdw, VARIANT, 9);
+            write(fdw, &varianta, sizeof(varianta));
+            write(fdw, VALUE, 7);
         }
-        //if(strncmp(buff,"VARIANT$",9)==0){
-        char text[]= "VARIANT$";
-        char text2[]="VALUE$";
-        char five='5';
-        write(fdw, &text, 9);
-        write(fdw, &five,1);
-        write(fdw, &varianta,1);
-        write(fdw, &text2, 7);
-            //printf("%s\n",text);
-        //}
-        
+        if (strstr(msg, "EXIT$"))
+        {
+            close(fdr);
+            close(fdw);
+            unlink(FILE_WRITE);
+            break;
+        }
+        break;
     }
-    
-    
-   
-    // if(strstr(mesaj, "EXIT")) {		
-    //         // munmap(&shared_mem, 2878785);
-    // 	    // munmap(data, dim_fisier);
-    //         // shm_unlink("/nJx8bhQ3");
-    //         close(fd1);		           
-    //         close(fdr);
-    //         close(fdw);
-    //         unlink(FILE_WRITE);
-    //     }
-    
+
+    close(fdr);
+    close(fdw);
+    unlink(FILE_WRITE);
+
     return 0;
 }
